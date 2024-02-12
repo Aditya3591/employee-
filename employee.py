e3d36779047ebffcc521e73ee496f165bac7f012
Aditya3591 <adityasinghi12345@gmail.com>
2024-02-12 17:04:40 +0530

    [aditya] added:montly wage

--- employee.py
+++ employee.py
@@ -8,6 +8,8 @@ class Employee_wage():
         self.full_hour=8
         self.part_time=4
         self.total_wage = 0
+        self.month_work_days=20
+        self.monthly_wage=0
 
 
     def check_attendance(self):
@@ -23,10 +25,19 @@ class Employee_wage():
             self.total_wage = self.wage_per_hour * self.part_time
         else:
             print('Employee is absent')
+        return self.total_wage
 
+    def emplyoee_month(self):
+        emp_day_wage=self.emplyoee_wage()
+        if emp_day_wage is not None:
+            self.monthly_wage=emp_day_wage * self.month_work_days
+            print(f"emplyoee month wage is : {self.monthly_wage}")
+    
 
         
 emp1=Employee_wage()
 
 emp1.emplyoee_wage()
-print(emp1.total_wage)
+print(f"per day wage {emp1.total_wage}")
+
+emp1.emplyoee_month()
