8b7636ea6b2b72befeebe5dbc8e1ec912b9706ef
Aditya3591 <adityasinghi12345@gmail.com>
2024-02-12 16:20:46 +0530

    [aditya] added:cal emplyoee wage

--- employee.py
+++ employee.py
@@ -2,15 +2,31 @@ import random
  
 class Employee_wage():
  
+    def __init__(self):
     
+        self.wage_per_hour=20
+        self.full_hour=8
+       
+        self.total_wage = 0
+
+
     def check_attendance(self):
 
         attendance= random.choice(['present', 'absent'])
-        print(attendance)
-        if attendance == 'present':
-            print("employee is present")
+        return attendance
+    
+    def emplyoee_wage(self):
+        emp_status = self.check_attendance()
+        if emp_status == 'present':
+            self.total_wage = self.wage_per_hour * self.full_hour
+        
         else:
-            print("emplyoee is absent")
+            print('Employee is absent')
+
+
         
 att=Employee_wage()
-att.check_attendance()
+# att.check_attendance()
+# worked_hour=int(input("enter the hours worked:"))
+att.emplyoee_wage()
+print(att.total_wage)
