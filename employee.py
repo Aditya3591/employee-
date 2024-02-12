8f99b81288445bc6c5f81dfd0acf92be49b8bb07
Aditya3591 <adityasinghi12345@gmail.com>
2024-02-12 16:28:49 +0530

    [aditya] added:part time emplyoee wage

--- employee.py
+++ employee.py
@@ -6,27 +6,27 @@ class Employee_wage():
     
         self.wage_per_hour=20
         self.full_hour=8
-       
+        self.part_time=4
         self.total_wage = 0
 
 
     def check_attendance(self):
 
-        attendance= random.choice(['present', 'absent'])
+        attendance= random.choice(['present', 'absent','halfday'])
         return attendance
     
     def emplyoee_wage(self):
         emp_status = self.check_attendance()
         if emp_status == 'present':
             self.total_wage = self.wage_per_hour * self.full_hour
-        
+        elif emp_status == 'halfday':
+            self.total_wage = self.wage_per_hour * self.part_time
         else:
             print('Employee is absent')
 
 
         
-att=Employee_wage()
-# att.check_attendance()
-# worked_hour=int(input("enter the hours worked:"))
-att.emplyoee_wage()
-print(att.total_wage)
+emp1=Employee_wage()
+
+emp1.emplyoee_wage()
+print(emp1.total_wage)
