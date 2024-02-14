7edf3f689aa3b8a26d8e3e4c26ecf31d56449cb0
Aditya3591 <adityasinghi12345@gmail.com>
2024-02-14 16:34:29 +0530

    [aditya] added:list of emplyoee salary of a company

--- employee.py
+++ employee.py
@@ -1,9 +1,9 @@
 import random
  
-class Employee_wage():
+class Employee_wage:
  
-    def __init__(self):
-    
+    def __init__(self, emp_name):
+        self.emp_name = emp_name
         self.wage_per_hour=20
         self.full_hour=8
         self.part_time=4
@@ -34,11 +34,50 @@ class Employee_wage():
         while self.total_days_worked < self.month_work_days and self.total_hours_worked<self.monthly_target_hours:
             self.emplyoee_wage()
             self.total_days_worked += 1
+
+    
+    
+    def __repr__(self) -> str:
+        return f'Emp salary: {self.total_wage} total days worked:{self.total_days_worked} total hours worked: {self.total_hours_worked}'
+
     
      
-        
-emp1=Employee_wage()
+class Company:
 
-emp1.monthly_wage()
-print(f"Total wage is: {emp1.total_wage}\ntotal hours works is : {emp1.total_hours_worked}\ntotal days worked is : {emp1.total_days_worked}")
+    def __init__(self, name):
+        self.company_name = name
+        self.employee_list={}
 
+    def add_employee(self,emp_obj):
+        self.employee_list[emp_obj.emp_name] = emp_obj
+
+    def display(self):
+        pass
+
+
+if __name__ == '__main__':
+
+    company = Company('SRM')
+    while True:
+        choice = int(input("choice: "))
+        if choice == 0:
+            break
+        emp_name = input("Emp name: ")
+        emp = Employee_wage(emp_name)
+        emp.monthly_wage()
+        company.add_employee(emp)
+
+        print(company.employee_list)
+     #print(__name__)
+     #emp=Employee_wage('Aditya')
+     #emp.monthly_wage()
+     #print(f"Total wage is: {emp.total_wage}\ntotal hours works is : {emp.total_hours_worked}\ntotal days worked is : {emp.total_days_worked}")
+     #company = Company('SRM')
+     #company.add_employee(emp)
+     #print(company.employee_list)
+
+
+
+    
+
+     
